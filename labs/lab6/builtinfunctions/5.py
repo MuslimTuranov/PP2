381030895bdc7207cc56a42e47257dce525e11e2
Muslim Turanov <muslimturanov25@outlook.com>
2024-03-07 11:26:48 +0500

    lab6

---
+++ labs/lab6/builtinfunctions/5.py
@@ -0,0 +1,4 @@
+tple = ("muslim", 5)
+tple2 = ("Muslim", 0)
+
+print(all(tple2))   
---
+++ labs/lab6/dirandfiles/4.py
@@ -0,0 +1,3 @@
+with open(r"C:\Users\musli\Documents\pp2\PP2\labs\muslim.txt", 'r') as file:
+    x = len(file.readlines())
+    print(x)
